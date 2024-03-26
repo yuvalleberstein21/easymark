07b490c21d2825a756ccb96e3fad8423d58ef6e8
MailBIT Sales <yuvalleberstein21@gmail.com>
2024-03-26 18:31:37 +0200

    update aws

--- server/Routes/BusinessRoutes.js
+++ server/Routes/BusinessRoutes.js
@@ -9,11 +9,7 @@ const { ObjectId } = require('mongoose');
 const fs = require('fs');
 const AWS = require('aws-sdk');
 
-const s3 = new AWS.S3({
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    region: 'eu-north-1'
-});
+const s3 = new AWS.S3();
 
 
 // CREATE BUSINESS - PROTECTED
